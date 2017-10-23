a83ed5cacfb18772f492405e06d7773c321e6cf4
Hector Riquelme <hector.riquelme1169@gmail.com>
2017-10-23 20:41:30 -0300

    Commit inicial

---
+++ src/main/java/com/example/demo/AskMeApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskMeApplication.class, args);
+	}
+}
